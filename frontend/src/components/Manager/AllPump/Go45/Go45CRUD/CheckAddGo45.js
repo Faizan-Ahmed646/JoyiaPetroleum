66a312fc8a5de512257d36810a69d4bacaae7a58
faizan-ahmed646 <faizanahmed3742@gmail.com>
2023-08-06 13:55:25 +0500

    rede

--- frontend/src/components/Manager/AllPump/Go45/Go45CRUD/CheckAddGo45.js
+++ frontend/src/components/Manager/AllPump/Go45/Go45CRUD/CheckAddGo45.js
@@ -6,8 +6,7 @@ const CheckAddGo45 = () => {
     const [uData, setUData] = useState([]);
     
     const userData = async() =>{
-        let res = await fetch("https://backend.joyiapetroleum.com
-        /joiya/go45/data/reverse")
+        let res = await fetch("https://backend.joyiapetroleum.com/joiya/go45/data/reverse")
         res = await res.json();    
         setUData(res)
     }
