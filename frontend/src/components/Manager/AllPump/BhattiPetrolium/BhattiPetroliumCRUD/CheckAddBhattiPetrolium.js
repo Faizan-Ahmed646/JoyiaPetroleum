da939fad91c25770ee945fe38de6efe986e36ed6
faizan-ahmed646 <faizanahmed3742@gmail.com>
2023-08-06 13:52:43 +0500

    re

--- frontend/src/components/Manager/AllPump/BhattiPetrolium/BhattiPetroliumCRUD/CheckAddBhattiPetrolium.js
+++ frontend/src/components/Manager/AllPump/BhattiPetrolium/BhattiPetroliumCRUD/CheckAddBhattiPetrolium.js
@@ -6,8 +6,7 @@ const CheckAddBhattiPetrolium = () => {
     const [uData, setUData] = useState([]);
     
     const userData = async() =>{
-        let res = await fetch("https://backend.joyiapetroleum.com
-/joiya/bhattiPetrolium/data/reverse")
+        let res = await fetch("https://backend.joyiapetroleum.com/joiya/bhattiPetrolium/data/reverse")
         res = await res.json();    
         setUData(res)
     }
